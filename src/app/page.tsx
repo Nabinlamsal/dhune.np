5ccee9e8c659dc4a4f5416e3d747a46c516e904d
Nabinlamsal <lamsalnabin60@gmail.com>
2025-11-06 01:54:48 +0545

    feature: Hero Section and Navbar creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,39 @@
-
-
+// Home.jsx
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">FYP Starts
+    <div>
+      <div className="relative mt-10 h-screen bg-[url('/hero.jpg')] bg-cover bg-center bg-no-repeat px-10">
+        {/* Optional overlay for readability */}
+        <div className="absolute inset-0 bg-[#eaeaea] opacity-20"></div>
+
+        {/* Hero content */}
+        <div className="relative z-10 flex items-center h-full px-10">
+          <div className="w-4/7">
+            <p className="font-bold text-[#ebbc01] font-nunito text-base mb-5 sm:text-lg lg:text-xl max-w-md">
+              / Connect. Compare. Clean.
+            </p>
+            <h1 className="text-4xl font-bold text-black sm:text-4xl lg:text-6xl">
+              Pickup to Delivery,
+              <div className="relative inline-flex">
+                <span className="absolute inset-x-0 bottom-0 border-b-[15px] border-blue-600"></span>
+                <h1 className="relative text-2xl font-bold text-black sm:text-4xl lg:text-6xl mt-4">Your Way!</h1>
+              </div>
+            </h1>
+            <p className="mt-8 text-xl font-bold text-black mb-8 sm:text-md">"Nepal’s First Platform Connecting You to Multiple Laundry Providers. Dhune.np is for costumers, vendors and business solving the traditional problems."</p>
+            <div>
+
+            </div>
+            <a href="#" title="" className="inline-flex items-center justify-center px-10 py-4  mr-10 text-base font-semibold text-white transition-all duration-200 bg-[#040947] hover:bg-[#121008ea] rounded-lg" role="button"> Get Started </a>
+            <a href="#" title="" className="inline-flex items-center justify-center px-10 py-4 text-base font-semibold text-white transition-all duration-200 bg-[#ebbc01] hover:bg-[#121008ea] rounded-lg" role="button"> Vendor Registration </a>
+
+          </div>
+        </div>
+      </div>
+      <div className="h-50 bg-blue-600 w-full text-white">
+        <h1>Second div</h1>
+
+      </div>
     </div>
+
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,26 @@
+// Navbar.jsx
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 flex items-center w-full justify-between bg-gradient-to-b from-green-200 to-green-50 md:h-15 h-10 py-4 text-gray-800 font-sans font-semibold px-8">
-
-      <div className="flex items-center space-x-2">
-        <img src="/1.png" alt="logo" className="h-8 w-8 mix-blend-multiply" />
-        <h6 className="font-semibold">Dhune.np</h6>
+    <nav className="fixed top-0 left-0 w-full z-50 flex items-center justify-between h-16 bg-[#F7F5EE] text-[#1A1A2E] font-sans font-semibold px-8 shadow-md">
+      <div className="flex items-center space-x-2 px-10">
+        <img src="/image.png" alt="logo" className="h-10 w-10 mix-blend-multiply" /><span>Dhune.np</span>
       </div>
 
-      <div className="flex flex-grow justify-center">
-        <div className="flex items-center space-x-8">
-          <a href="#" className="hover:text-green-600 transition">About</a>
-          <a href="#" className="hover:text-green-600 transition">Services</a>
-          <a href="#" className="hover:text-green-600 transition">FAQs</a>
-          <a href="#" className="hover:text-green-600 transition">Contact</a>
-          <a href="#" className="hover:text-green-600 transition">Blogs</a>
+      <div className="hidden md:flex justify-center">
+        <div className="flex items-left space-x-8">
+          <a href="#" className="hover:text-blue-600 transition">About</a>
+          <a href="#" className="hover:text-blue-600 transition">Services</a>
+          <a href="#" className="hover:text-blue-600 transition">FAQs</a>
+          <a href="#" className="hover:text-blue-600 transition">Contact</a>
+          <a href="#" className="hover:text-blue-600 transition">Blogs</a>
         </div>
       </div>
 
       <div>
-        <button className="bg-green-700 text-white px-4 py-1 rounded-lg hover:bg-green-700 transition">
+        <button className="bg-[] text-[#F7F5EE]  px-4 py-1 rounded-lg bg-blue-600 hover:bg-[#121008ea] transition">
           Book Service
         </button>
       </div>
-
     </nav>
   );
 }
