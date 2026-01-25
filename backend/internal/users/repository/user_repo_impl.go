679ce874ce163f588d599c2e664cb14a9976cc13
Nabinlamsal <lamsalnabin60@gmail.com>
2026-01-25 23:43:12 +0545

    update/repo: added implementation for user repo

--- backend/internal/users/repository/user_repo_impl.go
+++ backend/internal/users/repository/user_repo_impl.go
@@ -5,6 +5,7 @@ import (
 
 	db "github.com/Nabinlamsal/dhune.np/internal/database"
 	"github.com/Nabinlamsal/dhune.np/internal/users/model"
+	"github.com/google/uuid"
 )
 
 type UserRepoImpl struct {
@@ -56,4 +57,56 @@ func (repo *UserRepoImpl) GetUsersFiltered(ctx context.Context, roles []string, 
 	}
 
 	return users, nil
+}
+
+func (repo *UserRepoImpl) GetUserDetails(ctx context.Context, userId uuid.UUID) (*model.UserProfile, error) {
+	row, err := repo.q.GetUserDetail(ctx, userId)
+	if err != nil {
+		return nil, err
+	}
+	docs, err := repo.q.GetUserDocuments(ctx, userId)
+	if err != nil {
+		return nil, err
+	}
+
+	//mapping profile
+	profile := &model.UserProfile{
+		ID:          userId,
+		DisplayName: row.DisplayName,
+		Email:       row.Email,
+		Phone:       row.Phone,
+		Role:        row.Role,
+		Documents:   make([]model.Document, 0, len(docs)),
+		IsActive:    row.IsActive,
+		CreatedAt:   row.CreatedAt,
+		IsVerified:  row.IsVerified,
+	}
+
+	if row.BusinessProfileID.Valid {
+		profile.BusinessProfile = &model.BusinessProfile{
+			OwnerName:          row.BusinessOwnerName.String,
+			BusinessType:       row.BusinessType.String,
+			RegistrationNumber: row.BusinessRegistrationNumber.String,
+			ApprovalStatus:     row.BusinessApprovalStatus.String,
+		}
+	}
+	if row.VendorProfileID.Valid {
+		profile.VendorProfile = &model.VendorProfile{
+			OwnerName:          row.VendorOwnerName.String,
+			Address:            row.VendorAddress.String,
+			RegistrationNumber: row.VendorRegistrationNumber.String,
+			ApprovalStatus:     row.VendorApprovalStatus.String,
+		}
+	}
+
+	for _, document := range docs {
+		profile.Documents = append(profile.Documents, model.Document{
+			ID:           document.ID,
+			DocumentType: document.DocumentType,
+			DocumentURL:  document.DocumentUrl,
+			Verified:     document.Verified,
+			CreatedAt:    document.CreatedAt,
+		})
+	}
+	return profile, nil
 }
---
+++ backend/internal/users/service/user_command_service.go
@@ -0,0 +1,7 @@
+package service
+
+import "github.com/Nabinlamsal/dhune.np/internal/auth/repository"
+
+type CommandUserService struct {
+	commandRepo repository.CommandSer
+}
