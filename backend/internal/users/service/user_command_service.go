c63dc29cefcfb4ed4c986669c925935a169590b6
Nabinlamsal <lamsalnabin60@gmail.com>
2026-01-25 23:45:20 +0545

    feat/service: added service for command and user

--- backend/internal/users/service/user_command_service.go
+++ backend/internal/users/service/user_command_service.go
@@ -1,7 +1,90 @@
 package service
 
-import "github.com/Nabinlamsal/dhune.np/internal/auth/repository"
+import (
+	"context"
+	"errors"
 
-type CommandUserService struct {
-	commandRepo repository.CommandSer
+	"github.com/google/uuid"
+)
+
+func (s *UserService) ApproveVendor(ctx context.Context, userId uuid.UUID) error {
+	user, err := s.userRepo.GetUserDetails(ctx, userId)
+	if err != nil {
+		return err
+	}
+	if user.VendorProfile.ApprovalStatus != "pending" {
+		return errors.New("user must be in pending state")
+	}
+
+	if user.Role != "vendor" {
+		return errors.New("user is not a vendor")
+	}
+	return s.commandRepo.ApproveVendor(ctx, userId)
+}
+func (s *UserService) RejectVendor(ctx context.Context, userId uuid.UUID) error {
+	user, err := s.userRepo.GetUserDetails(ctx, userId)
+	if err != nil {
+		return err
+	}
+	if user.VendorProfile.ApprovalStatus != "pending" {
+		return errors.New("user must be in pending state")
+	}
+
+	if user.Role != "vendor" {
+		return errors.New("user is not a vendor")
+	}
+	return s.commandRepo.RejectVendor(ctx, userId)
+}
+
+func (s *UserService) ApproveBusinessUser(ctx context.Context, userId uuid.UUID) error {
+	user, err := s.userRepo.GetUserDetails(ctx, userId)
+	if err != nil {
+		return err
+	}
+	if user.VendorProfile.ApprovalStatus != "pending" {
+		return errors.New("user must be in pending state")
+	}
+
+	if user.Role != "business" {
+		return errors.New("user is not a business")
+	}
+	return s.commandRepo.ApproveBusinessUser(ctx, userId)
+}
+func (s *UserService) RejectBusinessUser(ctx context.Context, userId uuid.UUID) error {
+	user, err := s.userRepo.GetUserDetails(ctx, userId)
+	if err != nil {
+		return err
+	}
+	if user.VendorProfile.ApprovalStatus != "pending" {
+		return errors.New("user must be in pending state")
+	}
+
+	if user.Role != "business" {
+		return errors.New("user is not a business")
+	}
+	return s.commandRepo.RejectBusinessUser(ctx, userId)
+}
+
+func (s *UserService) SuspendUser(ctx context.Context, userId uuid.UUID) error {
+	user, err := s.userRepo.GetUserDetails(ctx, userId)
+	if err != nil {
+		return err
+	}
+	if user.Role == "admin" {
+		return errors.New("you cant suspend admin")
+	}
+	if user.IsActive == false {
+		return errors.New("user is already suspended or inactive")
+	}
+	return s.commandRepo.SuspendUser(ctx, userId)
+}
+func (s *UserService) ReactivateUser(ctx context.Context, userId uuid.UUID) error {
+	user, err := s.userRepo.GetUserDetails(ctx, userId)
+	if err != nil {
+		return err
+	}
+	if user.IsActive == true {
+		return errors.New("user is already active")
+	}
+	return s.commandRepo.ReactivateUser(ctx, userId)
 }
--- backend/internal/users/service/user_service.go
+++ backend/internal/users/service/user_service.go
@@ -1,1 +1,21 @@
 package service
+
+import (
+	"context"
+
+	"github.com/Nabinlamsal/dhune.np/internal/users/model"
+	"github.com/Nabinlamsal/dhune.np/internal/users/repository"
+)
+
+type UserService struct {
+	commandRepo repository.CommandRepository
+	userRepo    repository.UserRepository
+}
+
+func NewUserService(commandRepo repository.CommandRepository, userRepo repository.UserRepository) *UserService {
+	return &UserService{commandRepo: commandRepo, userRepo: userRepo}
+}
+
+func (service *UserService) GetUsersFiltered(ctx context.Context, roles []string, status *string, search *string) *[]model.AdminUserSummary {
+
+}
