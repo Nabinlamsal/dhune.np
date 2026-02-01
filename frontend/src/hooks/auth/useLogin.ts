36d21667d6173004d3059666834c6266dcdacd0d
Nabinlamsal <lamsalnabin60@gmail.com>
2026-02-01 23:05:28 +0545

    update(auth):update hooks to tanstack query for login

--- frontend/src/hooks/auth/useLogin.ts
+++ frontend/src/hooks/auth/useLogin.ts
@@ -1,31 +1,11 @@
-import { useState } from "react";
-import { login } from "../../services/auth.service";
-import { LoginRequest, LoginResponse } from "../../types/auth/login";
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { login } from "@/src/services/auth.service";
+import { LoginRequest, LoginResponse } from "@/src/types/auth/login";
 
 export const useLogin = () => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-
-    const loginUser = async (
-        payload: LoginRequest
-    ): Promise<LoginResponse> => {
-        setLoading(true);
-        setError(null);
-
-        try {
-            const response = await login(payload);
-            return response;
-        } catch (err: any) {
-            setError(err?.message || "Login failed");
-            throw err;
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    return {
-        login: loginUser,
-        loading,
-        error,
-    };
+    return useMutation<LoginResponse, Error, LoginRequest>({
+        mutationFn: (payload: LoginRequest) => login(payload),
+    });
 };
