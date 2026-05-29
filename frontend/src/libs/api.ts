6035c1ce0f305bc421c4b3b863df1e0a21104cb3
Nabinlamsal <lamsalnabin60@gmail.com>
2026-05-29 20:36:44 +0545

    fix api url name issue

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8111";
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
