637633772c0a1aa743bb1f92381a5e3bc68e9246
Nabinlamsal <lamsalnabin60@gmail.com>
2026-05-29 20:19:51 +0545

    update local and production api isse for deployment

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8111";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
