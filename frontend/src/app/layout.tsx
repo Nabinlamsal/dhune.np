e79ca45d7133fbcbabe94a35462e1fea7a355fb8
Nabinlamsal <lamsalnabin60@gmail.com>
2026-06-15 15:24:49 +0545

    fix import error in layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Nunito } from "next/font/google";
+import "./globals.css"
 
 import { Providers } from "./providers";
 import { Toaster } from "sonner"
